b11478b7aa3f621c23a0cd6d25e184c6417227b5
Nicolas <nicolas.piirainen@gmail.com>
2026-06-13 21:37:53 +0300

    Distance to water now accurate to shoreline

--- Assets/Scripts/Audio/DistanceToWater.cs
+++ Assets/Scripts/Audio/DistanceToWater.cs
@@ -1,4 +1,6 @@
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Splines;
 
 
 public class DistanceToWater : MonoBehaviour
@@ -6,18 +8,48 @@ public class DistanceToWater : MonoBehaviour
     [Header("Parameter Change")]
     [SerializeField] private string parameterName;
 
-    [Range(0, 50)]
+    [Range(0, 100)]
     [SerializeField] float parameterDistance;
     private float parameterValue;
 
-
     [Header("Player")]
-    [SerializeField] private Transform playerPos;
+    [SerializeField] private Transform player;
+
+
+    [Header("Spline")]
+    [SerializeField] private SplineContainer splineContainer;
+    private Spline spline;
+
+    void Awake()
+    {
+        spline = splineContainer.Spline;
+
+        if(player == null)
+        {
+            player = FindAnyObjectByType<CharacterController>().transform;
+        }
+    }
 
     void Update()
     {
-        transform.position = new Vector3(playerPos.position.x, transform.position.y, transform.position.z);
-        var distanceToWater = Mathf.Abs(transform.position.z - playerPos.position.z);
+
+        Vector3 localSplinePoint = 
+            splineContainer.transform.InverseTransformPoint(player.position);
+
+        SplineUtility.GetNearestPoint(
+            spline, 
+            localSplinePoint, 
+            out float3 nearestPoint, 
+            out _);
+            
+        Vector3 nearestWorldPosition = splineContainer.transform.TransformPoint(nearestPoint);
+
+        Debug.Log(nearestWorldPosition);
+
+        transform.position = nearestWorldPosition;
+
+        // transform.position = new Vector3(playerPos.position.x, transform.position.y, transform.position.z);
+        var distanceToWater = Mathf.Abs(Vector3.Distance(player.position, transform.position));
 
         parameterValue = Mathf.InverseLerp(40, 0, distanceToWater);
         // Debug.Log(parameterValue);
