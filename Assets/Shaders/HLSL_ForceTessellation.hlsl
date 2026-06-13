bcd712cbf0fd99981b4bce3c01a69a770dd989b0
CarelessAntics <6470546+CarelessAntics@users.noreply.github.com>
2026-06-13 15:10:56 +0300

    Actual fix for tessellation shader error

--- Assets/Shaders/HLSL_ForceTessellation.hlsl
+++ Assets/Shaders/HLSL_ForceTessellation.hlsl
@@ -3,7 +3,7 @@
 #pragma hull hull
 #pragma domain domain
 
-void ForceTess_float(in float2 UV, in float3 WorldPos, in float3 WorldNormal, out float Dummy)
+void ForceTess_float(in float2 UV, in float3 WorldPos, in float3 WorldNormal, in float3 WorldTangent, out float Dummy)
 {
     Dummy = 0;
 }
