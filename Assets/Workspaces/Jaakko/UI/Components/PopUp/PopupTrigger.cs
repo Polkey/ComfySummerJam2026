ad3afd3565d6e4c8eaee5e573ffae8cf02a784c6
jmzn00 <mansikkajaakko13@gmail.com>
2026-06-18 04:41:36 +0300

    Tutorial Popup

--- Assets/Workspaces/Jaakko/UI/Components/PopUp/PopupTrigger.cs
+++ Assets/Workspaces/Jaakko/UI/Components/PopUp/PopupTrigger.cs
@@ -4,11 +4,25 @@ public class PopupTrigger : MonoBehaviour
 {
     [SerializeField] private UIV_PopUP m_view;
 
+    [SerializeField] private float m_duration;
+    [SerializeField] private string m_text;
+    [SerializeField] private Sprite m_sprite;
+    [SerializeField] private bool m_activateOnce;
+
+    private bool activated = false;
+
     private void OnTriggerEnter(Collider other)
     {
         if (!other.CompareTag("MainCamera")) return;
+        if (m_activateOnce && activated) return;
 
-        m_view.Bind(PopupDatabase.T_Zoom);
+        m_view.Bind(new TutorialPopupData() 
+        {
+            duration = m_duration,
+            text = m_text,
+            sprite = m_sprite
+        });
         m_view.Show();
+        activated = true;
     }    
 }
