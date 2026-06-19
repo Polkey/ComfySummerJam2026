fa35f825002dae6b91c08cecc37d2e4ce50d1e19
Yrjö Peussa <peussa@iki.fi>
2026-06-19 18:04:49 +0300

    bonfire vfx connected to game logic, effect is placeholder tho

--- Assets/Workspaces/Yka/BonfireVFXController.cs
+++ Assets/Workspaces/Yka/BonfireVFXController.cs
@@ -1,9 +1,36 @@
+using System;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class BonfireVFXController : MonoBehaviour
 {
+    [SerializeField] private int bonfireStage;
+    [SerializeField] private int maxStage = 3;
+    [Header("Debug display only")]
+    [SerializeField] private float percent;
+
+    private VisualEffect vfx;
+
+    void Awake()
+    {
+        vfx = GetComponent<VisualEffect>();
+    }
     public void SetBonfireStage(int stage)
     {
-        // TODO
+        float per = (float)stage / maxStage;
+        SetBonfirePercent(per);
+    }
+
+    void SetBonfirePercent(float percent)
+    {
+        this.percent = percent;
+        vfx.SetFloat("PercentActive", percent);
+    }
+
+    private void OnValidate()
+    {
+        if (!vfx)
+            vfx = GetComponent<VisualEffect>();
+        SetBonfireStage(bonfireStage);
     }
 }
