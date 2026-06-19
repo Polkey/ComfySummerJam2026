473154fa1f5b547765c6bce1457a3a457970971c
Yrjö Peussa <peussa@iki.fi>
2026-06-19 21:17:17 +0300

    Dynamic light in bonfire

--- Assets/Workspaces/Yka/BonfireVFXController.cs
+++ Assets/Workspaces/Yka/BonfireVFXController.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.VFX;
@@ -6,18 +5,25 @@ using UnityEngine.VFX;
 public class BonfireVFXController : MonoBehaviour {
     [SerializeField] private int bonfireStage;
     [SerializeField] private int maxStage = 3;
+    [SerializeField] private AnimationCurve lightCurve;
+    [SerializeField] private float maxLightIntensity;
+    [SerializeField] private float wobbleSpeedFactor = 4;
+    [Range(0, 1)][SerializeField] private float lightWobblePercent = 0.2f;
     [Header("Debug display only")]
     [SerializeField] private float percent;
 
     private VisualEffect[] vfx;
+    private Light light;
+    private float currentIntensityLevel;
 
     void Awake() {
         vfx = GetComponentsInChildren<VisualEffect>();
+        light = GetComponentInChildren<Light>();
         SetBonfireStage(0);
     }
     public void SetBonfireStage(int stage) {
         bonfireStage = stage;
-        float per = (float)stage / maxStage;
+        float per = Mathf.Clamp01((float)stage / maxStage);
         SetBonfirePercent(per);
     }
 
@@ -26,9 +32,14 @@ public class BonfireVFXController : MonoBehaviour {
         foreach (var effect in vfx) {
             effect.SetFloat("PercentActive", percent);
         }
+        currentIntensityLevel = lightCurve.Evaluate(percent) * maxLightIntensity;
     }
 
     private void Update() {
+        var wobble = (Mathf.PerlinNoise1D(Time.time * wobbleSpeedFactor) * 2 - 1) * lightWobblePercent;
+        light.intensity = Mathf.Max(0, currentIntensityLevel * (1 + wobble));
+        
+        // DEBUG
         if (Keyboard.current.digit1Key.wasPressedThisFrame) {
             SetBonfireStage(0);
         }
